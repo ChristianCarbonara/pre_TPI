d858cfa1a18dc5ac5b8bfc3c156f367d7c5bf93b
Christian Carbonara <christcarbo@hotmail.com>
2018-04-27 14:00:48 +0200

    Version test 1.0
    27.04.2018
    
    Test de la première version

---
+++ test.cs
@@ -0,0 +1,63 @@
+﻿/// ETML
+/// Auteur : Christian Carbonara
+/// Date   : 00.00.0000
+/// Description : ...
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/// <summary>
+/// 
+/// </summary>
+namespace WindowsFormsApp1
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class fnt : Form
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        string valueBox;
+        
+        public fnt()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Text = "Convertir";
+            label2.Text = "nombre chiffre : -";
+            label3.Text = "2ème chiffre : -";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            valueBox = textBox1.Text;
+            label1.Text = textBox1.Text + "," + textBox2.Text;
+            label2.Text = "nombre chiffre: " + Convert.ToString(textBox1.TextLength);
+            
+            label3.Text = "nombre chiffre: " + valueBox.Substring(1,1);
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.BackColor = Color.DeepPink;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.BackColor = Color.MediumSpringGreen;
+        }
+    }
+}
